ce69c2e5257687573225f4e4f8b861f811729b9c
tiagobuck <tiago.buck45@gmail.com>
2022-11-15 11:41:40 -0300

    correcion

--- script.js
+++ script.js
@@ -1,82 +1,72 @@
-
 function Producto(info){
     this.nombre = info.nombre;
     this.precio = info.precio;
-  
+    
 };
+
 let producto1 = new Producto({
     nombre: "hilos",
-    precio: 300,
-   
+    precio: 3100,
+ 
 });
 
-let producto2 =new Producto({
+let producto2 = new Producto({
     nombre: "elastico",
-    precio: 100,
+    precio: 1000,
     
 });
 
 let producto3 = new Producto({
     nombre: "guata",
-    precio: 600,
+    precio: 1500,
    
 });
 
+const catalogo = [producto1, producto2, producto3];
+
+
+
 alert(`Bienvenidos a la tienda online!
         La lista de productos es:
-        Producto - Precio
-        ${producto1.nombre} - $ ${producto1.precio} 
-        ${producto2.nombre} - $ ${producto2.precio} 
-        ${producto3.nombre} - $ ${producto3.precio} 
+        Producto - Precio - Peso
+        ${catalogo[0].nombre} - $ ${catalogo[0].precio} - ${catalogo[0].peso}kg
+        ${catalogo[1].nombre} - $ ${catalogo[1].precio} - ${catalogo[1].peso}kg
+        ${catalogo[2].nombre} - $ ${catalogo[2].precio} - ${catalogo[2].peso}kg
         
         `);
 
-let comprarProducto1 = prompt(`¿Quiere comprar el producto ${producto1.nombre} a $ ${producto1.precio}? Su respuesta debe ser si o no. En caso contrario deberá ingresar nuevamente la respuesta.`).toLowerCase();
+const respuesta = [];
 
-while ((comprarProducto1 != "si") && (comprarProducto1 != "no")) {
-    comprarProducto1 = prompt(`Su respuesta es incorrecta. Vuelva a ingresar una respuesta para el producto ${producto1.nombre}.`);
-}
 
-let comprarProducto2 = prompt(`¿Quiere comprar el producto ${producto2.nombre} a $ ${producto2.precio}? Su respuesta debe ser si o no. En caso contrario deberá ingresar nuevamente la respuesta.`).toLowerCase();
-
-while ((comprarProducto2 != "si") && (comprarProducto2 != "no")) {
-    comprarProducto2 = prompt(`Su respuesta es incorrecta. Vuelva a ingresar una respuesta para el producto ${producto2.nombre}.`);
-}
-
-let comprarProducto3 = prompt(`¿Quiere comprar el producto ${producto3.nombre} a $ ${producto3.precio}? Su respuesta debe ser si o no. En caso contrario deberá ingresar nuevamente la respuesta.`).toLowerCase();
-
-while ((comprarProducto3 != "si") && (comprarProducto3 != "no")) {
-    comprarProducto3 = prompt(`Su respuesta es incorrecta. Vuelva a ingresar una respuesta para el producto ${producto3.nombre}.`);
+for (let index = 0; index < catalogo.length; index++) {
+    let comprarProducto1 = prompt(`¿Quiere comprar el producto ${catalogo[index].nombre} a $ ${catalogo[index].precio}? Su respuesta debe ser si o no. En caso contrario deberá ingresar nuevamente la respuesta.`).toLowerCase();
+    while ((comprarProducto1 != "si") && (comprarProducto1 != "no")) {
+        comprarProducto1 = prompt(`Su respuesta es incorrecta. Vuelva a ingresar una respuesta para el producto ${catalogo[index].nombre}.`);
+    }    
+    respuesta.push(comprarProducto1);
 }
 
 function compra() {
     let carrito = 0;
-    
-    if (comprarProducto1 === "si") {
-        carrito = carrito + producto1.precio;
-       
+  
+
+    for (let index = 0; index < catalogo.length; index++) {
+        if (respuesta[index] === "si"){
+            carrito = carrito + catalogo[index].precio;
+           
+        }
     }
 
-    if (comprarProducto2 === "si") {
-        carrito = carrito + producto2.precio;
-       
-    }
-
-
-    if (comprarProducto3 === "si") {
-        carrito = carrito + producto3.precio;
-       
-    }
 
    
-    let costoTotal = carrito ;
+    let costoTotal = carrito;
 
     if (carrito != 0) {
         alert(`Resumen de la compra
-Subtotal - $ ${carrito}
-
---------------------------
-Total - $ ${costoTotal}`);
+                Subtotal - $ ${carrito}
+              
+                --------------------------
+                Total - $ ${costoTotal}`);
         alert("Fin de la compra.");
     } else {
         alert("Dado que no ha agregado ningún producto a su carrito, usted no ha realizado ninguna compra. Gracias por su tiempo.")
